50d709cf7b04a8895cf9bb85ff9d3cefa80657c3
Iblasio Sanchez <Iblasio.sanchez@gmail.com>
2021-01-26 23:12:04 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/IblasioS418.github.io/precache-manifest.c8ea77a1af316c7063e043887828de4a.js"
+  "/precache-manifest.7d539f69a1b4faaa49b93d3d321a1d62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/IblasioS418.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
